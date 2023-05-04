e35687958ea9491adfe184ab50004a8b941dc516
Edward Gilbert <egbot@asu.edu>
2023-05-04 09:51:46 -0700

    Developments
    - Add determination update statement to schema patch

--- config/schema/3.0/dev/db_schema_patch-3.1.sql
+++ config/schema/3.0/dev/db_schema_patch-3.1.sql
@@ -1,2 +1,15 @@
 INSERT INTO schemaversion (versionnumber) values ("3.1");
 
+
+
+
+# Transfer current determinations from omoccurrences table
+INSERT IGNORE INTO omoccurdeterminations(occid, identifiedBy, dateIdentified, family, sciname, verbatimIdentification, scientificNameAuthorship, tidInterpreted, 
+identificationQualifier, genus, specificEpithet, verbatimTaxonRank, infraSpecificEpithet, isCurrent, identificationReferences, identificationRemarks, 
+taxonRemarks)
+SELECT occid, IFNULL(identifiedBy, "unknown"), IFNULL(dateIdentified, "s.d."), family, IFNULL(sciname, "undefined"), scientificName, scientificNameAuthorship, tidInterpreted, identificationQualifier, 
+genus, specificEpithet, taxonRank, infraSpecificEpithet, 1 as isCurrent, identificationReferences, identificationRemarks, taxonRemarks
+FROM omoccurrences;
+
+
+
