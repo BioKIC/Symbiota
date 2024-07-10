7682ce5f407310f5b3d9c3d749affba4d9fd22c5
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-07-09 17:22:29 -0700

    Change quicksearch again

--- index.php
+++ index.php
@@ -28,13 +28,13 @@ header('Content-Type: text/html; charset=' . $CHARSET);
 	?>
 	<div class="navpath"></div>
 	<main id="innertext">
-		<div style="float:right;width:600px;margin-left:20px">
+		<div style="float:right;width:700px;margin-left:20px">
 			<div id="quicksearchdiv">
 				<!-- -------------------------QUICK SEARCH SETTINGS--------------------------------------- -->
 				<form name="quicksearch" id="quicksearch" action="<?php echo $CLIENT_ROOT; ?>/taxa/index.php" method="get" onsubmit="return verifyQuickSearch(this);">
 						<div id="quicksearchtext" ><?php echo (isset($LANG['QSEARCH_SEARCH'])?$LANG['QSEARCH_SEARCH']:'Search Taxon'); ?></div>
 						<input id="taxa" type="text" name="taxon" />
-						<button name="formsubmit"  id="quicksearchbutton" type="submit" value="Search Terms"><?php echo (isset($LANG['QSEARCH_SEARCH_BUTTON'])?$LANG['QSEARCH_SEARCH_BUTTON']:'Search'); ?></button>
+						<button name="formsubmit"  id="quicksearchbutton" type="submit" value="Search Terms" style="margin-bottom:10px"><?php echo (isset($LANG['QSEARCH_SEARCH_BUTTON'])?$LANG['QSEARCH_SEARCH_BUTTON']:'Search'); ?></button>
 				</form>
 			</div>
 			<div>
