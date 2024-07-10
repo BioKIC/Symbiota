f96d76f8ffb855e9654acb0ed1ef32e9f48076f4
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-07-09 17:26:26 -0700

    Update index.php

--- index.php
+++ index.php
@@ -29,9 +29,9 @@ header('Content-Type: text/html; charset=' . $CHARSET);
 	<div class="navpath"></div>
 	<main id="innertext">
 		<div style="float:right;width:500px;margin-left:20px">
-			<div id="quicksearchdiv">
+			<div id="quicksearchdiv" style="width:600px">
 				<!-- -------------------------QUICK SEARCH SETTINGS--------------------------------------- -->
-				<form style="width:700px" name="quicksearch" id="quicksearch" action="<?php echo $CLIENT_ROOT; ?>/taxa/index.php" method="get" onsubmit="return verifyQuickSearch(this);">
+				<form name="quicksearch" id="quicksearch" action="<?php echo $CLIENT_ROOT; ?>/taxa/index.php" method="get" onsubmit="return verifyQuickSearch(this);">
 						<div id="quicksearchtext" ><?php echo (isset($LANG['QSEARCH_SEARCH'])?$LANG['QSEARCH_SEARCH']:'Search Taxon'); ?></div>
 						<input id="taxa" type="text" name="taxon" />
 						<button name="formsubmit"  id="quicksearchbutton" type="submit" value="Search Terms" style="margin-bottom:10px;margin-top:10px"><?php echo (isset($LANG['QSEARCH_SEARCH_BUTTON'])?$LANG['QSEARCH_SEARCH_BUTTON']:'Search'); ?></button>
